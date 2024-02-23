4e299f342432718755a0ff3b2ff4757ce6fdc7b5
anosha25 <124650395+anosha25@users.noreply.github.com>
2024-02-22 23:08:07 -0500

    !

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,33 @@
-var canvas;
-var ctx;
-var w = 500;
-var h = 400;
+// var canvas;
+// var ctx;
+// var w = 500;
+// var h = 400;
 
-setUpCanvas();
-background();
+// setUpCanvas();
+// background();
 
-function setUpCanvas() {
-    canvas = document.querySelector("#myCanvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = w;
-    canvas.height = h;
-    canvas.style.border = "2px dotted red";
-}
+// function setUpCanvas() {
+//     canvas = document.querySelector("#myCanvas");
+//     ctx = canvas.getContext("2d");
+//     canvas.width = w;
+//     canvas.height = h;
+//     canvas.style.border = "2px dotted red";
+// }
 
-function background(){
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(0, 0, w, h);
-    }  
+// function background(){
+//     ctx.fillStyle = "#FFF";
+//     ctx.fillRect(0, 0, w, h);
+//     }  
 
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(w/2, h);
-    ctx.lineTo(w,0);
-    ctx.stroke();
+//     ctx.beginPath();
+//     ctx.moveTo(0, 0);
+//     ctx.lineTo(w/2, h);
+//     ctx.lineTo(w,0);
+//     ctx.stroke();
+
+var sticky = new Sticky('.sticky');
+
+
 
 
 console.log("Resource Web!!");
